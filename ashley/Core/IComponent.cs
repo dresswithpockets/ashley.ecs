e33fbb2cfa5845c9f369b2ae3304e40410f5b0fb
Tristen Horton <tristen.horton@verramobility.com>
2020-03-06 13:28:00 -0700

    add code doc for IComponent type

--- ashley/Core/IComponent.cs
+++ ashley/Core/IComponent.cs
@@ -1,7 +1,11 @@
 ﻿namespace ashley.Core
 {
+    /// <summary>
+    /// A Component is intended as a data holder and provides data to be processed in an <see cref="EntitySystem"/>.
+    /// 
+    /// Implement this interface to flag a class as a component.
+    /// </summary>
     public interface IComponent
     {
-        
     }
 }
