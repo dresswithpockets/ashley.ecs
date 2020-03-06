da240e9fecd129ea96a43be1663283a29e0b1c47
Tristen Horton <tristen.horton@verramobility.com>
2020-03-06 15:07:56 -0700

    add code doc to ComponentType

--- ashley/Core/ComponentType.cs
+++ ashley/Core/ComponentType.cs
@@ -4,6 +4,12 @@ using ashley.Utils;
 
 namespace ashley.Core
 {
+    /// <summary>
+    /// Uniquely identifiers a <see cref="IComponent"/> implementing class. It assigns them an index which is used
+    /// internally for fast comparison and retrieval. See <see cref="Family"/> and <see cref="Entity"/>. Access
+    /// ComponentTypes for a specified type using <see cref="GetFor{T}"/> or <see cref="GetFor"/>. Each unique type
+    /// will always return the same instance of ComponentType.
+    /// </summary>
     public class ComponentType
     {
         private static Dictionary<Type, ComponentType> _assignedComponentTypes =
@@ -15,11 +21,20 @@ namespace ashley.Core
 
         public int Index => _index;
 
-        public ComponentType()
+        private ComponentType()
         {
             _index = _typeIndex++;
         }
 
+        /// <summary>
+        /// returns a ComponentType matching the specified runtime type
+        /// </summary>
+        /// <param name="componentType">The runtime type to get a ComponentType for</param>
+        /// <returns>a ComponentType matching the specified runtime type</returns>
+        /// <exception cref="ArgumentException">
+        /// thrown when <paramref name="componentType"/> does not represent an instantiable class or is not an
+        /// IComponent type.
+        /// </exception>
         public static ComponentType GetFor(Type componentType)
         {
             if (!typeof(IComponent).IsAssignableFrom(componentType) || !componentType.IsClass)
@@ -35,12 +50,38 @@ namespace ashley.Core
             return type;
         }
 
+        /// <summary>
+        /// returns a ComponentType matching the specified type
+        /// </summary>
+        /// <typeparam name="T">
+        /// The class which implements <see cref="IComponent"/> get the representative ComponentType for.
+        /// </typeparam>
+        /// <returns>a ComponentType matching the specified type</returns>
         public static ComponentType GetFor<T>() where T : class, IComponent => GetFor(typeof(T));
 
+        /// <summary>
+        /// Helper method. Short form of <code>ComponentType.GetFor(typeof(ComponentClass)).Index</code>
+        /// </summary>
+        /// <param name="componentType">The runtime type to get a matching ComponentType's index for.</param>
+        /// <returns>The index for the matching ComponentType</returns>
         public static int GetIndexFor(Type componentType) => GetFor(componentType).Index;
 
+        /// <summary>
+        /// Helper method. Short form of <code>ComponentType.GetFor&lt;T&gt;().Index</code>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>The index for the matching ComponentType</returns>
         public static int GetIndexFor<T>() where T : class, IComponent => GetIndexFor(typeof(T));
 
+        /// <summary>
+        /// returns a <see cref="BitSet"/> representing the collection of components for quick comparison and matching.
+        /// See <see cref="Family"/>
+        /// </summary>
+        /// <param name="componentTypes">component types to get a representative bitset for</param>
+        /// <returns>
+        /// a <see cref="BitSet"/> representing the collection of components for quick comparison and matching.
+        /// See <see cref="Family"/>
+        /// </returns>
         public static BitSet GetBitsFor(params Type[] componentTypes)
         {
             var bitArray = new BitSet(componentTypes.Length);
